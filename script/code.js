925f313898b0dfda98ce2a248c11629b79c44e97
siyamanga22 <zwenisiyamanga@gmail.com>
2023-05-17 09:22:31 +0200

    objectAndArray

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let person ={
+    name: 'Siya',
+    lastName:'Zweni',
+    age: 22
+};
+let person2 ={
+    name:'Chocolate',
+    lastName:'Mpongwana',
+    age: 22,
+};
+console.log(person);
+console.log(person2);
+let arr =[];
+arr.push(person); //added person object content to the arra 'arr
+console.log(arr); //displayed person content inside array
+//done number 6 opened dev tools everything showed! wala!
